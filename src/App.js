d91c45a303e6174389183ef6250cba3422a3772d
youngwook-kwon <ywkwon11@gmail.com>
2023-09-08 17:44:57 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 const customers = [{
   'id': 1,
@@ -28,24 +34,26 @@ const customers = [{
 ]
 
 function App() {
+  
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-          <Customer
-            key = {c.id}
-            id = {c.id}
-            image = {c.image}
-            name = {c.name}
-            birthday = {c.birthday}
-            gender = {c.gender}
-            job = {c.job}
-          />
-          );
-        })
-      }
-    </div>
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => {return (<Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> ); }) }
+        </TableBody>
+      </Table>
+      
+    </Paper>
   );
   
 }
